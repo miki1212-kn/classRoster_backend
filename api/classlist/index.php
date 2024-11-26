f16681542e5ebba9d852e34df70f897745f1a225
mk <kanzakimiki.12@gmail.com>
2024-11-26 13:13:00 +0900

    first commit

---
+++ api/classlist/index.php
@@ -0,0 +1,33 @@
+<?php
+
+const DB_HOST = "localhost";
+const DB_USER = "mkanzaki";
+const DB_PASSWORD = "miki1212";
+const DB_DRIVER = "mysql";
+const DB_NAME = "classroster";
+const DB_DSN = DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME;
+
+try{
+  $db = new PDO(DB_DSN,DB_USER,DB_PASSWORD);
+  $sql = "SELECT * FROM class_list";
+
+
+  $stmt = $db -> prepare($sql);
+  $stmt -> execute();
+  //連想配列のみかえす
+  while ($row = $stmt -> fetchAll(PDO::FETCH_ASSOC)){
+    $classList[] = $row;
+  }
+}
+catch(PDOException $e){
+  print $e -> getMessage();
+
+};
+
+var_dump($classList); //かえってきた
+
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+print json_encode($classList);
